30120a2fbee22d16f21d405e1b0fe50cfee90a66
Dark <hafidhdark@gmail.com>
2026-09-09 11:54:06 +0100

    Updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -45,6 +45,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 0.6,
     },
+    {
+      url: `${baseUrl}/messi-moves-into-spanish-club-ownership-as-eldense-takeover-nears-completion`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
     ...articleUrls,
   ];
 }
